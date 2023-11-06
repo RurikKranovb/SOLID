c7eeb6ef22d311ae63f2eca3d471bb5ecf2bf01f
bobrov.da <dmr.bobrov@yandex.ru>
2023-11-06 18:33:50 +0300

    Add LiskovSubstitutionPrinciple

---
+++ LiskovSubstitutionPrinciple/Infrastructure/Square.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitutionPrinciple.Infrastructure
+{
+    internal class Square : Core.Rectangle
+    {
+        public override int Width
+        {
+            get => base.Width; set
+            {
+                base.Width = value;
+                base.Height = value;
+            }
+        }
+        public override int Height
+        {
+            get => base.Height; set
+            {
+                base.Height = value;
+                base.Width = value;
+            }
+        }
+    }
+}
---
+++ LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,38 @@
+﻿using LiskovSubstitutionPrinciple.Infrastructure;
+using LiskovSubstitutionPrinciple.Core;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        LiskovSubstitutionPrinciple.Core.Rectangle rect = new Square();
+        TestRectangleArea(rect);
+
+        Console.Read();
+    }
+
+    //public static void TestRectangleArea(LiskovSubstitutionPrinciple.Core.Rectangle rect)
+    //{
+    //    rect.Height = 5;
+    //    rect.Width = 10;
+    //    if (rect.GetArea() != 50)
+    //        throw new Exception("Некорректная площадь!");
+    //}
+
+    public static void TestRectangleArea(Rectangle rect)
+    {
+        if (rect is Square)
+        {
+            rect.Height = 5;
+            if (rect.GetArea() != 25)
+                throw new Exception("Неправильная площадь!");
+        }
+        else if (rect is Rectangle)
+        {
+            rect.Height = 5;
+            rect.Width = 10;
+            if (rect.GetArea() != 50)
+                throw new Exception("Неправильная площадь!");
+        }
+    }
+}
